7ddad395cdbde6da8f1879e51a4dc23df65b64c9
piotrekbass <piotrek.koscielniak@gmail.com>
2014-02-25 21:55:41 +0100

    init

---
+++ app.js
@@ -0,0 +1,14 @@
+function *getAllSquareNumbers() {
+    for (var i = 1; ; i++) {
+        // Every time we 'yield', this function's execution pauses until
+        // the generator is restarted by a call to 'next' (see below).
+        yield i * i;
+    }
+}
+
+// Now let's fetch some values
+var generator = getAllSquareNumbers();
+console.log(generator.next().value); // Outputs '1'
+console.log(generator.next().value); // Outputs '4'
+console.log(generator.next().value); // Outputs '9'
+console.log(generator.next().value); // Outputs '16'
